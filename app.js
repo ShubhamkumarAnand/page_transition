3f7eaaf21c3a705f516e6f7146d9dc8066cc40cb
imskanand <the.anand3099@gmail.com>
2023-04-10 19:40:08 +0530

    feat/Leave_animation: Done for each component

--- app.js
+++ app.js
@@ -21,7 +21,19 @@ const leaveAnimation = (current, done) => {
       { y: 100, opacity: 0, onComplete: done },
       "<"
     ),
-    tlLeave.fromTo(text, { y: 0, opacity: 1 }, { opacity: 0, y: 100 }, "<")
+    tlLeave.fromTo(text, { y: 0, opacity: 1 }, { opacity: 0, y: 100 }, "<"),
+    tlLeave.fromTo(
+      circles,
+      { y: 0, opacity: 1 },
+      {
+        y: -200,
+        opacity: 0,
+        stagger: 0.15,
+        ease: "back.out(1.7)",
+        duration: 1,
+      },
+      "<"
+    )
   );
 };
 
